8869d8c3f09292c371c21bbc0ff1494a821f519a
code-fun-mx <andy0506.seon@gmail.com>
2025-09-20 11:04:54 +0900

    Add files via upload

--- study_timer.py
+++ study_timer.py
@@ -1,14 +1,20 @@
 import streamlit as st
 import time
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title = '공부용 타이머', page_icon = '📖')
+
+
+
 
 st. title('📖공부 타이머⏰')
 st.divider()
 
 #m = st.number_input('**시간을 입력해 주세요(분)**', min_value = 15, value = 30, max_value = 125, step = 5)
 time_set = st.empty()
+breaktime = st.empty()
 st.session_state['timer_time'] = time_set.number_input('**시간을 입력해 주세요(분)**', min_value = 15, value = 30, max_value = 125, step = 5)
+st.session_state['breaktime'] = breaktime.number_input('**쉴 시간을 입력해주세요**', min_value = 5, value = 10, max_value = 20, step = 5)
 
 timer_hours = st.session_state.timer_time//60
 st.session_state['timer_hours'] = timer_hours
@@ -17,6 +23,7 @@ st.session_state['timer_mins'] = timer_mins
 timer_secs = 00
 st.session_state['timer_secs'] = timer_secs
 
+
 stop_hours = 0
 st.session_state['stop_hours'] = stop_hours
 stop_mins = 0
@@ -24,25 +31,39 @@ st.session_state['stop_mins'] = stop_mins
 stop_secs = 0
 st.session_state['stop_secs'] = stop_secs
 
-#st.title(f'{hours:02}:{mins:02}.')
-st.markdown(f"<h1 style='text-align: center;', style = 'font-size: 300px;'>{timer_hours:02}:{timer_mins:02}:{timer_secs:02}</h1>",
-    unsafe_allow_html=True)
-#st.button('**타이머 시작!**')
-#st.write(f'**총 공부 시간: {stop_hours:02}:{stop_mins:02}:{stop_secs:02}**')
-#st.write(f'**총 공부 시간: {st.session_state.stop_hours:02}:{st.session_state.stop_mins:02}:{st.session_state.stop_secs:02}**')
+breaktime_secs = 0
+st.session_state['breaktime_secs'] = breaktime_secs
+
+
+timer  = st.empty()
+timer.markdown(f"<h1 style='text-align: center;', style = 'font-size: 300px;'>{st.session_state.timer_hours:02}:{st.session_state.timer_mins:02}:{st.session_state.timer_secs:02}</h1>", unsafe_allow_html=True)
 stopwatch = st.empty()
 stopwatch.write(f'**총 공부 시간: {st.session_state.stop_hours:02}:{st.session_state.stop_mins:02}:{st.session_state.stop_secs:02}**')
+st.divider()
+
+st.header('**쉬는 시간**')
+breaktimer = st.empty()
+breaktimer.markdown(f"<h1 style='text-align: center;', style = 'font-size: 300px;'>{st.session_state.breaktime:02}:{st.session_state.breaktime_secs:02}</h1>", unsafe_allow_html=True)
+
+
+
 
 if st.button('**타이머 시작!**'):
     time_set.empty()
+    breaktime.empty()
     while True:
+        if st.session_state.timer_secs==0:
+            st.session_state.timer_secs = 60
+            st.session_state.timer_mins -= 1
+            if st.session_state.timer_mins == 0:
+                if st.sessions_state.timer_hours == 0:
+                    break
         time.sleep(1)
         st.session_state.stop_secs+=1
         st.session_state.timer_secs-=1
-        if st.session_state.timer.secs==0:
-            st.session_state.timer.secs == 59
-            st.session_state.timer_mins -= 1
-            
+        
+        
+        timer.markdown(f"<h1 style='text-align: center;', style = 'font-size: 300px;'>{st.session_state.timer_hours:02}:{st.session_state.timer_mins:02}:{st.session_state.timer_secs:02}</h1>", unsafe_allow_html=True)
 
 
         stopwatch.write(f'**총 공부 시간: {st.session_state.stop_hours:02}:{st.session_state.stop_mins:02}:{st.session_state.stop_secs:02}**')
@@ -54,11 +75,3 @@ if st.button('**타이머 시작!**'):
             st.session_state.stop_hours += 1
             st.session_state.stop_mins = 0
             stopwatch.write(f'**총 공부 시간: {st.session_state.stop_hours:02}:{st.session_state.stop_mins:02}:{st.session_state.stop_secs:02}**')
-
-        
-
-
-        
-
-
-            
